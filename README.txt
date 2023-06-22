de88df7129bb43dcb8e5b3a65ff0aaa59a9b08ba
sahil8841 <64150483+sahil8841@users.noreply.github.com>
2023-06-22 22:26:29 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+I have mentoined all the requirements you asked for in the code
+I ahve compresed the main folder to upload it on github
+please uncompress it and then test it
---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE products(
+id INT NOT NULL PRIMARY KEY IDENTITY,
+product VARCHAR (100) NOT NULL,
+about VARCHAR (200) NOT NULL,
+created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO products (product,about)
+VALUES
+('Iphone11','good phone with camera')
